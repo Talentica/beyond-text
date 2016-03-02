3bcf9c0cc447daa04566616c13aa7f9782e41fd7
Nikita <nikita@nikitap.talentica-all.com>
2016-03-02 11:46:51 +0530

    Avro to parquet and text conversion map-reduce code.

--- src/main/java/com/avro/AvroToParquetConversion.java
+++ src/main/java/com/avro/AvroToParquetConversion.java
@@ -1,10 +1,14 @@
+/*Avro to Parquet conversion code */
+
 package com.avro;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapreduce.AvroKeyInputFormat;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -20,60 +24,60 @@ import parquet.avro.AvroParquetOutputFormat;
 import parquet.avro.AvroSchemaConverter;
 import parquet.hadoop.metadata.CompressionCodecName;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-//import parquet.org.apache.thrift.protocol.TBinaryProtocol.Factory;
-
-
 public class AvroToParquetConversion extends Configured implements Tool {
 
-    //static String rawschema;
-    public static class AvroMapper extends
-            Mapper<AvroKey<GenericRecord>, NullWritable, Void, GenericRecord> {
-        @Override
-        protected void map(AvroKey<GenericRecord> key, NullWritable value,
-                           Context context) throws IOException, InterruptedException {
-            context.write(null, key.datum());
-        }
-    }
+	public static class AvroMapper extends
+			Mapper<AvroKey<GenericRecord>, NullWritable, Void, GenericRecord> {
+		@Override
+		protected void map(AvroKey<GenericRecord> key, NullWritable value,
+				Context context) throws IOException, InterruptedException {
+			context.write(null, key.datum());
+		}
+	}
 
-    public int run(String[] args) throws Exception {
-        Path schemaPath = new Path(args[0]);
-        Path inputPath = new Path(args[1]);
-        Path outputPath = new Path(args[2]);
-        Job job = new Job(getConf());
-        job.setJarByClass(getClass());
-        Configuration conf = job.getConfiguration();
-        FileSystem fs = FileSystem.get(conf);
-        InputStream in = fs.open(schemaPath);
-        Schema avroSchema = new Schema.Parser().parse(in);
-        System.out.println(new AvroSchemaConverter().convert(avroSchema).toString());
-        FileInputFormat.addInputPath(job, inputPath);
-        job.setInputFormatClass(AvroKeyInputFormat.class);
-        job.setOutputFormatClass(AvroParquetOutputFormat.class);
-        AvroParquetOutputFormat.setOutputPath(job, outputPath);
-        AvroParquetOutputFormat.setSchema(job, avroSchema);
-        AvroParquetOutputFormat.setCompression(job, CompressionCodecName.SNAPPY);
-        AvroParquetOutputFormat.setCompressOutput(job, true);
-        /* Impala likes Parquet files to have only a single row group.
-        * Setting the block size to a larger value helps ensure this to
-        * be the case, at the expense of buffering the output of the
-        * entire mapper's split in memory.
-        *
-        * It would be better to set this based on the files' block size,
-        * using fs.getFileStatus or fs.listStatus.
-        */
-        AvroParquetOutputFormat.setBlockSize(job, 500 * 1024 * 1024);
-        job.setMapperClass(AvroMapper.class);
-        job.setNumReduceTasks(0);
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+	public int run(String[] args) throws Exception {
+		
+		Path schemaPath = new Path(args[0]);
+		Path inputPath = new Path(args[1]);
+		Path outputPath = new Path(args[2]);
+		Job job = new Job(getConf());
+		job.setJarByClass(getClass());
+		Configuration conf = job.getConfiguration();
+		FileSystem fs = FileSystem.get(conf);
+		InputStream in = fs.open(schemaPath);
+		
+		/* Parse in-built avro file schema */
+		Schema avroSchema = new Schema.Parser().parse(in);
+		System.out.println(new AvroSchemaConverter().convert(avroSchema)
+				.toString());
+		FileInputFormat.addInputPath(job, inputPath);
+		job.setInputFormatClass(AvroKeyInputFormat.class);
+		
+		/*Use avro to parquet output format to generate parquet file  */
+		job.setOutputFormatClass(AvroParquetOutputFormat.class);
+		AvroParquetOutputFormat.setOutputPath(job, outputPath);
+		AvroParquetOutputFormat.setSchema(job, avroSchema);
+		AvroParquetOutputFormat
+				.setCompression(job, CompressionCodecName.SNAPPY);
+		AvroParquetOutputFormat.setCompressOutput(job, true);
+		/*
+		 * Impala likes Parquet files to have only a single row group. Setting
+		 * the block size to a larger value helps ensure this to be the case, at
+		 * the expense of buffering the output of the entire mapper's split in
+		 * memory.
+		 * 
+		 * It would be better to set this based on the files' block size, using
+		 * fs.getFileStatus or fs.listStatus.
+		 */
+		AvroParquetOutputFormat.setBlockSize(job, 500 * 1024 * 1024);
+		job.setMapperClass(AvroMapper.class);
+		job.setNumReduceTasks(0);
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new AvroToParquetConversion(), args);
 
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new AvroToParquetConversion(), args);
-
-        System.exit(exitCode);
-    }
+		System.exit(exitCode);
+	}
 }
